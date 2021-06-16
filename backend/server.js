d771b367f20430d76c533eaf4ead9dfd51c91595
Bruno Austin <brunoaugustine2@gmail.com>
2021-06-16 14:58:29 +0100

    add error handling

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,12 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/middleware.js'
 
 dotenv.config()
 
 connectDB()
+
 const app = express()
 
 app.get('/', (req, res) => {
@@ -14,6 +16,12 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+// error handling
+app.use(notFound)
+
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
