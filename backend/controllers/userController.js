355cf2a9b824f7dc484a8e8f8d7b80e26bc68627
Bruno Austin <brunoaugustine2@gmail.com>
2021-12-06 08:40:08 +0100

    add register route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,7 +11,7 @@ const authUser = asyncHandler(async (req, res) => {
  const user = await User.findOne({ email })
  if (user && (user.matchPasseord(password))) {
     res.json({
-        _id: usjuuunner._id,
+        _id: user._id,
         name: user.name,
         email: user.email,
         isAdmin: user.isAdmin,
@@ -23,4 +23,51 @@ const authUser = asyncHandler(async (req, res) => {
  }
 })
 
-export { authUser}
+// @desc        REgister a new user
+// @route       POST /api/users
+// @access      Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+ 
+  const userExists = await User.findOne({ email })
+  if (userExists) {
+     res.status(400)
+     throw new Error('User already exists')
+  } 
+
+  const user = await User.create({ name, email, password})
+  if(user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user_id)
+      })
+  } else {
+      res.status(400)
+      throw new Error('Invalid user data')
+  }
+ })
+
+
+// @desc        get user profile
+// @route       GET /api/users/profile
+// @access      Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+ })
+
+export { authUser, getUserProfile, registerUser }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import { authUser, getUserProfile, registerUser } from '../controllers/userController.js'
+import { protect } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
-import { authUser } from '../controllers/userController.js'
-
+router.route('/').post(registerUser)
 router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
